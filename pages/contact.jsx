113b31caa8a96a74a5ab173c8a3e039bd68c9273
Pedro Specter <pedro.specter@nortb.com>
2022-11-06 22:55:20 +0000

    ff

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
+import { AiOutlineMail } from 'react-icons/ai'
+import { BsFillPhoneVibrateFill } from 'react-icons/bs'
+import { GiRotaryPhone } from 'react-icons/gi'
 import Footer from '../components/footer'
 import Header from '../components/header'
 import SEO from '../components/seo'
 
 export default function Contact() {
     return (
-        <div>
+        <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
             <SEO
                 title="DSB | contact us"
                 desc="Contact us to get the best advise about consulting and financial planning " />
             <Header />
+            <div className="h-100" style={{ flex: 1 }}>
+                <div className="container">
+                    <h1 className="py-5" style={{ fontWeight: 600 }}>Love to hear from you,
+                        <br />get in <b className="color1">touch</b> &#128075;</h1>
+                    <p>You can reach us through one of these channels:</p>
+                    <ul style={{ listStyle: "none", padding: 0 }}>
+                        <li><AiOutlineMail /> diannesheilabona@yahoo.com</li>
+                        <li><BsFillPhoneVibrateFill /> +63 (0) 9178254525</li>
+                        <li><GiRotaryPhone /> +63 (2) 7577-8040</li>
+                    </ul>
+                </div>
+            </div>
             <Footer />
         </div>
     )
--- pages/services.jsx
+++ pages/services.jsx
@@ -5,12 +5,108 @@ import SEO from '../components/seo'
 
 export default function Services() {
     return (
-        <div>
+        <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
             <SEO
-                title="DSB | consulting services"
-                desc="We care about providing the best care for your business"
+                title="DSB | about us"
+                desc="we help with Tax Filing, Book Keeping and Audit of different companies coming from several industries"
             />
             <Header />
+
+            <section className="body" style={{ flex: 1 }}>
+
+                <section className="container py-5">
+                    <div className="row">
+                        <div className="col-12 col-md-6 h-100">
+                            <h2 style={{ fontSize: 40, fontWeight: 600 }}>We make your <br/>  <span className="color1">  Business Interesting</span></h2>
+                        </div>
+                        <div className="col-12 col-md-6 d-flex justify-content-start align-items-start  ">
+                            <span>Make your business ledger more clean and unique in the eyes of each of your customers to reach a wider market.
+                            </span>
+                        </div>
+                    </div>
+                </section>
+
+                <section className=" py-5">
+                    <img src="https://images.unsplash.com/photo-1554224154-26032ffc0d07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1652&q=100" alt="bookkeeping, tax-filing and others" width="100%" height={300} style={{ objectFit: "cover", objectPosition: "center" }} />
+                </section>
+                <section className="container ">
+                    <hr />
+                </section>
+                <section className="container py-5">
+                    <div className="row">
+                        <div className="col-12 col-md-6 h-100">
+                            <h2 style={{ fontSize: 40, fontWeight: 600 }}>Together we <br /> are strong</h2>
+                        </div>
+                        <div className="col-12 col-md-6 d-flex flex-column justify-content-start align-items-start  ">
+                            <p style={{ fontWeight: 500, fontSize: 20 }}>Our crew is always getting bigger, but we all work toward one goal: to make bookkeeping not only lean and clean but inevitable for every client that works with us</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem illo saepe tempore necessitatibus voluptates vel quod. Inventore provident dignissimos saepe dolor assumenda, accusamus labore, vel amet praesentium quaerat dolore earum.</p>
+                        </div>
+                    </div>
+                </section>
+                <section className="container ">
+                    <hr />
+                </section>
+
+                <section>
+                    <div className="container py-5">
+                        <div className="row">
+                            <div className="col-12 col-md-4">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nemo ratione odio sequi labore exercitationem neque vitae aliquid cumque, mollitia necessitatibus ullam iste voluptate? Alias temporibus totam perspiciatis assumenda iure?</p>
+                                <span>230+</span>
+                            </div>
+                            <div className="col-12 col-md-4">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nemo ratione odio sequi labore exercitationem neque vitae aliquid cumque, mollitia necessitatibus ullam iste voluptate? Alias temporibus totam perspiciatis assumenda iure?</p>
+                                <span>230+</span>
+                            </div>
+                            <div className="col-12 col-md-4">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nemo ratione odio sequi labore exercitationem neque vitae aliquid cumque, mollitia necessitatibus ullam iste voluptate? Alias temporibus totam perspiciatis assumenda iure?</p>
+                                <span>230+</span>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section>
+                    <div className="container bg-light p-5 mb-3">
+                        <div className="row">
+                            <div className="col-12 col-md-6">
+                                <h2>Meet our <br />amazing team</h2>
+                            </div>
+                        </div>
+                        <hr />
+                        <div className="row">
+                            <div className="col-12 col-md-3">
+                                <span style={{ width: "100%", height: 300, backgroundColor: "#ddd", display: "inline-flex" }}></span>
+                                <p>Diane Sheila</p>
+                            </div>
+                            <div className="col-12 col-md-3">
+                                <span style={{ width: "100%", height: 300, backgroundColor: "#ddd", display: "inline-flex" }}></span>
+                                <p>Diane Sheila</p>
+                            </div>
+                            <div className="col-12 col-md-3">
+                                <span style={{ width: "100%", height: 300, backgroundColor: "#ddd", display: "inline-flex" }}></span>
+                                <p>Diane Sheila</p>
+                            </div>
+                            <div className="col-12 col-md-3">
+                                <span style={{ width: "100%", height: 300, backgroundColor: "#ddd", display: "inline-flex" }}></span>
+                                <p>Diane Sheila</p>
+                            </div>
+                        </div>
+                        <hr />
+                        <div className="row">
+                            <div className="col-12 col-md-6">
+                                <h2>Join our team</h2>
+                            </div>
+                            <div className="col-12 col-md-6">
+                                <p>We believe it takes great people to make a great roduct. That's why we hire not only the perfect professional fits, but people who embody our company values. </p>
+                                <a href="mailto:diannesheilabona@yahoo.com" className=""><u>Contact us today</u></a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </section>
+
             <Footer />
         </div>
     )
